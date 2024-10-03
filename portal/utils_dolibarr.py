3acfc0280fa6245388e8d5050039960a07581516
joelbtactic <joel.ampurdanes@btactic.com>
2024-10-03 16:05:58 +0200

    feat: add get_listview_filter method to obtain the filters in the list view

--- portal/utils_dolibarr.py
+++ portal/utils_dolibarr.py
@@ -13,3 +13,16 @@ class DolibarrUtils:
     def __init__(self) -> None:
         self.dolibarr_service = DolibarrApiService()
         self.dolibarr_cached = DolibarrApiServiceCached()
+
+    def get_listview_filter(self, parameters):
+        filters = parameters.copy()
+        # List of keys to remove
+        keys_to_remove = ['limit', 'offset', 'order_by', 'order', 'csrfmiddlewaretoken']
+        for key in keys_to_remove:
+            if key in filters:
+                del filters[key]
+        # Iterate over a copy of the keys to avoid modifying the dictionary during iteration
+        for key in list(filters.keys()):
+            if not filters[key]:
+                del filters[key]
+        return filters
