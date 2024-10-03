da453004e1f61cc02a24c4ca7c60dcaa57a54e5d
joelbtactic <joel.ampurdanes@btactic.com>
2024-10-03 11:31:37 +0200

    feat: add utility function for date range queries and improve datetime handling for date query filters

--- portal/utils.py
+++ portal/utils.py
@@ -540,56 +540,83 @@ def get_related_user_records(module, user):
         return None
     return records
 
+def get_calendar_days_in_sql_format(option, days, field_name):
+    filter_date = datetime.now()
+    today = filter_date.strftime("%Y-%m-%d")
+
+    if option == 'this_month':
+        first_month = filter_date.replace(day=1).strftime("%Y-%m-%d")
+        second_month = filter_date.replace(day=1, month=filter_date.month+1).strftime("%Y-%m-%d")
+        query = field_name  + ' >= \'' + first_month + '\'' + ' AND ' + field_name  + ' < \'' + second_month + '\''
+
+    elif option == 'last':
+        last_days = filter_date - timedelta(days)
+        query = field_name  + ' >= \'' + last_days.strftime("%Y-%m-%d") + '\'' + ' AND ' + field_name  + ' < \'' + today + '\''
+
+    elif option == 'last_month':
+        first_month = filter_date.replace(day=1, month=filter_date.month-1).strftime("%Y-%m-%d")
+        second_month = filter_date.replace(day=1).strftime("%Y-%m-%d")
+        query = field_name  + ' >= \'' + first_month + '\'' + ' AND ' + field_name  + ' < \'' + second_month + '\''
+
+    elif option == 'next_month':
+        first_month = filter_date.replace(day=1, month=filter_date.month+1).strftime("%Y-%m-%d")
+        second_month = filter_date.replace(day=1, month=filter_date.month+2).strftime("%Y-%m-%d")
+        query = field_name  + ' >= \'' + first_month + '\'' + ' AND ' + field_name  + ' < \'' + second_month + '\''
+
+    elif option == 'last_year':
+        first_month = filter_date.replace(day=1, month=1, year=filter_date.year-1).strftime("%Y-%m-%d")
+        second_month = filter_date.replace(day=1, month=1).strftime("%Y-%m-%d")
+        query = field_name  + ' >= \'' + first_month + '\'' + ' AND ' + field_name  + ' < \'' + second_month + '\''
+
+    elif option == 'next_year':
+        first_month = filter_date.replace(day=1, month=1, year=filter_date.year+1).strftime("%Y-%m-%d")
+        second_month = filter_date.replace(day=1, month=1, year=filter_date.year+2).strftime("%Y-%m-%d")
+        query = field_name  + ' >= \'' + first_month + '\'' + ' AND ' + field_name  + ' < \'' + second_month + '\''
+
+    elif option == 'this_year':
+        first_month = filter_date.replace(day=1, month=1, year=filter_date.year).strftime("%Y-%m-%d")
+        second_month = filter_date.replace(day=1, month=1, year=filter_date.year+1).strftime("%Y-%m-%d")
+        query = field_name  + ' >= \'' + first_month + '\'' + ' AND ' + field_name  + ' < \'' + second_month + '\''
+
+    else:
+        next_days = filter_date + timedelta(days)
+        query = field_name  + ' >= \'' + today + '\'' + ' AND ' + field_name  + ' < \'' + next_days.strftime("%Y-%m-%d") + '\''
+
+    return query
 
 def get_datetime_option_in_mysql_format(option, field_name, params=None):
     params = params or []
     if option == '=' and params[0]:
-        return 'DATE(' + field_name + ') = DATE(\'' + params[0] + '\')'
+        return field_name  + ' like \'%' + params[0] + '%\''
     if option == 'not_equal' and params[0]:
-        return 'DATE(' + field_name + ') != DATE(\'' + params[0] + '\')'
+        return field_name  + ' notlike \'%' + params[0] + '%\''
     if option == 'greater_than' and params[0]:
-        return 'DATE(' + field_name + ') > DATE(\'' + params[0] + '\')'
+        return field_name  + ' > \'' + params[0] + '\''
     if option == 'less_than' and params[0]:
-        return 'DATE(' + field_name + ') < DATE(\'' + params[0] + '\')'
+        return field_name  + ' < \'' + params[0] + '\''
     if option == 'between' and params[0] and params[1]:
-        return 'DATE(' + field_name + ') BETWEEN DATE(\'' + params[0] \
-            + '\') AND DATE(\'' + params[1] + '\')'
+        return field_name  + ' >= \'' + params[0] + '\'' + ' AND ' + field_name  + ' < \'' + params[1] + '\''
     if option == 'last_7_days':
-        return 'DATE(' + field_name + ') >= DATE_ADD(UTC_DATE(), INTERVAL - 6 DAY)' \
-            + ' AND ' + 'DATE(' + field_name + ') <= UTC_DATE()'
+        return get_calendar_days_in_sql_format("last", 7, field_name)
     if option == 'next_7_days':
-        return 'DATE(' + field_name + ') >= UTC_DATE() ' \
-            + ' AND ' + 'DATE(' + field_name + \
-            ') <= DATE_ADD(UTC_DATE(), INTERVAL + 6 DAY)'
+        return get_calendar_days_in_sql_format("next", 7, field_name)
     if option == 'last_30_days':
-        return 'DATE(' + field_name + ') >= DATE_ADD(UTC_DATE(), INTERVAL - 29 DAY)' \
-            + ' AND ' + 'DATE(' + field_name + ') <= UTC_DATE()'
+        return get_calendar_days_in_sql_format("last", 30, field_name)
     if option == 'next_30_days':
-        return 'DATE(' + field_name + ') >= UTC_DATE() ' \
-            + ' AND ' + 'DATE(' + field_name + \
-            ') <= DATE_ADD(UTC_DATE(), INTERVAL + 29 DAY)'
+        return get_calendar_days_in_sql_format("next", 30, field_name)
     if option == 'last_month':
-        return 'YEAR(' + field_name \
-            + ') = YEAR(DATE_ADD(UTC_DATE(), INTERVAL - 1 MONTH)) ' \
-            + 'AND MONTH(' + field_name \
-            + ') = MONTH(DATE_ADD(UTC_DATE(), INTERVAL - 1 MONTH))'
+        return get_calendar_days_in_sql_format("last_month", 0, field_name)
     if option == 'this_month':
-        return 'YEAR(' + field_name + ') = YEAR(UTC_DATE()) AND MONTH(' \
-            + field_name + ') = MONTH(UTC_DATE())'
+        return get_calendar_days_in_sql_format("this_month", 0, field_name)
     if option == 'next_month':
-        return 'YEAR(' + field_name \
-            + ') = YEAR(DATE_ADD(UTC_DATE(), INTERVAL + 1 MONTH)) ' \
-            + 'AND MONTH(' + field_name \
-            + ') = MONTH(DATE_ADD(UTC_DATE(), INTERVAL + 1 MONTH))'
+        return get_calendar_days_in_sql_format("next_month", 0, field_name)
     if option == 'last_year':
-        return 'YEAR(' + field_name \
-            + ') = YEAR(DATE_ADD(UTC_DATE(), INTERVAL - 1 YEAR))'
+        return get_calendar_days_in_sql_format("last_year", 0, field_name)
     if option == 'this_year':
-        return 'YEAR(' + field_name \
-            + ') = YEAR(UTC_DATE())'
+        return get_calendar_days_in_sql_format("this_year", 0, field_name)
     if option == 'next_year':
-        return 'YEAR(' + field_name \
-            + ') = YEAR(DATE_ADD(UTC_DATE(), INTERVAL + 1 YEAR))'
+         return get_calendar_days_in_sql_format("next_year", 0, field_name)
+
     return None
 
 
